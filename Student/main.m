a1917828b1a8a8f8c64f78fab59aed6cb8205449
rajkumarchalla <challarajkumarreddy@gmail.com>
2013-09-03 10:01:39 +0530

    Initial Commit

---
+++ Student/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Student
+//
+//  Created by TEKGLINT SOFTSOL on 9/3/13.
+//  Copyright (c) 2013 TEKGLINT SOFTSOL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
